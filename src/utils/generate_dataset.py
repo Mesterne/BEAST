937965e9d86160ded1fdcf851039021faa22ee9d
Hans Jakob Håland <hansjakob.haland99@gmail.com>
2025-01-30 15:57:31 +0100

    Adds step size for ts windows and plotly pca plot

--- src/utils/generate_dataset.py
+++ src/utils/generate_dataset.py
@@ -1,16 +1,20 @@
 from typing import List
 import pandas as pd
+from tqdm import tqdm
 
 
 def generate_windows_dataset(
-    data: pd.DataFrame, window_size: int, include_columns: List[str] = None
+    data: pd.DataFrame,
+    window_size: int,
+    step_size: int,
+    include_columns: List[str] = None,
 ) -> List[pd.DataFrame]:
     dataset = []
 
     if include_columns is not None:
         data = data[include_columns]
 
-    for i in range(len(data) - window_size + 1):
+    for i in tqdm(range(0, len(data) - window_size + 1, step_size), total=len(data)):
         dataset.append(data.iloc[i : i + window_size])
 
     return dataset
