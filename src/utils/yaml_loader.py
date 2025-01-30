f652ff38ff7ce3bccbf75003ef52ae08e5fbf859
Vegard Sjåvik <vgsjaavik@gmail.com>
2025-01-30 10:34:33 +0100

    🧪 Add experiments folder and yaml loader

---
+++ src/utils/yaml_loader.py
@@ -0,0 +1,9 @@
+import yaml
+
+def read_yaml(filename):
+    with open(f'{filename}','r') as f:
+        output = yaml.safe_load(f)
+    return output
+    
+config = read_yaml('experiments/gridloss/feedforward.yml')
+print(config['dataset_args'])
