74b79d62e806b922f8dc960fd9f379df84d65711
Vegard Sjåvik <vgsjaavik@gmail.com>
2025-02-10 08:44:17 +0100

    Fix bugs

--- src/utils/data_formatting.py
+++ src/utils/data_formatting.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def use_model_predictions_to_create_dataframe(predictions, TARGET_NAMES, target_dataframe):
+def use_model_predictions_to_create_dataframe(
+    predictions, TARGET_NAMES, target_dataframe
+):
     column_names = [col.replace("target_", "") for col in TARGET_NAMES]
     predictions_df = pd.DataFrame(predictions, columns=column_names)
     # FIXME: This logic does not work when we have validation and test set.
