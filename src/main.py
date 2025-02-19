bc023cc415816206cd8024f8d1cf5fa15e423977
Hans Jakob Håland <hansjakob.haland99@gmail.com>
2025-02-19 16:10:55 +0100

    Adds visualization of MTS and PCA, saved to PDF file

--- src/main.py
+++ src/main.py
@@ -49,6 +49,11 @@ from src.data_transformations.generation_of_supervised_pairs import (
 )
 from src.utils.genetic_algorithm import GeneticAlgorithm
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+from src.utils.pca import PCAWrapper
+
 # Setting seeds
 SEED = 42
 np.random.seed(SEED)
@@ -107,6 +112,9 @@ init_mts_decomps = mts_decomps[init_mts_index]
 init_mts_features = mts_feature_df.iloc[init_mts_index]
 init_uts = init_mts[init_uts_name]  # Series
 init_uts_decomp = init_mts_decomps[init_uts_index]
+
+print(init_mts.head(0))
+print(mts_dataset[init_mts_index].head(0))
 
 trend_det_factor = config["transformation_args"]["trend_det_factor"]
 trend_slope_factor = config["transformation_args"]["trend_slope_factor"]
@@ -185,6 +193,8 @@ logging.info("Successfully generated model input dataframe")
 
 # Predict features
 predicted_features = model.infer(model_input_df)
+# Get all columns that are not index columns in predicted_features df
+
 
 logging.info("Successfully predicted features")
 
@@ -209,20 +219,25 @@ trend_det_factor_high = config["genetic_algorithm_args"]["legal_values"][
     "trend_det_factor"
 ][1]
 trend_slope_factor_low = config["genetic_algorithm_args"]["legal_values"][
-    "trend_slope"
+    "trend_slope_factor"
 ][0]
 trend_slope_factor_high = config["genetic_algorithm_args"]["legal_values"][
-    "trend_slope"
+    "trend_slope_factor"
 ][1]
-trend_lin_factor_low = config["genetic_algorithm_args"]["legal_values"]["trend_lin"][0]
-trend_lin_factor_high = config["genetic_algorithm_args"]["legal_values"]["trend_lin"][1]
+trend_lin_factor_low = config["genetic_algorithm_args"]["legal_values"][
+    "trend_lin_factor"
+][0]
+trend_lin_factor_high = config["genetic_algorithm_args"]["legal_values"][
+    "trend_lin_factor"
+][1]
 seasonal_det_factor_low = config["genetic_algorithm_args"]["legal_values"][
-    "seasonal_det"
+    "seasonal_det_factor"
 ][0]
 seasonal_det_factor_high = config["genetic_algorithm_args"]["legal_values"][
-    "seasonal_det"
+    "seasonal_det_factor"
 ][1]
 
+# Contstraint on GA solutions
 legal_factor_values = [
     np.linspace(trend_det_factor_low, trend_det_factor_high, 100),
     np.linspace(trend_slope_factor_low, trend_slope_factor_high, 100),
@@ -230,11 +245,16 @@ legal_factor_values = [
     np.linspace(seasonal_det_factor_low, seasonal_det_factor_high, 100),
 ]
 
+is_index_column = predicted_features.columns.str.contains("index")
+predicted_features_reshape = (
+    predicted_features.loc[:, ~is_index_column].to_numpy().reshape(3, 4)
+)
+
 GA_runs_mts = []
 GA_runs_features = []
 GA_runs_factors = []
 
-
+logging.info("Starting genetic algorithm runs...")
 for _ in tqdm(range(num_GA_runs)):
 
     transformed_mts = []
@@ -256,7 +276,7 @@ for _ in tqdm(range(num_GA_runs)):
             continue
 
         univariate_decomps = init_mts_decomps[i]
-        univariate_target_features = predicted_features[i]
+        univariate_target_features = predicted_features_reshape[i]
 
         ga_instance = GeneticAlgorithm(
             original_time_series_decomp=univariate_decomps,
@@ -305,6 +325,173 @@ for _ in tqdm(range(num_GA_runs)):
     GA_runs_features.append(transformed_mts_features)
     GA_runs_factors.append(transformed_mts_factors)
 
+logging.info(
+    f"Successfully transformed the other univariate time series in the MTS. {num_GA_runs} genetic algorithm runs completed."
+)
+
+# Create pca spaces for 2D visualization
+# MTS PCA
+mts_pca_transformer = PCAWrapper()
+mts_pca_df = mts_pca_transformer.fit_transform(mts_feature_df)
+
+# One PCA for each UTS
+tot_num_mts_features = (
+    predicted_features_reshape.shape[0] * predicted_features_reshape.shape[1]
+)
+num_uts_features = (tot_num_mts_features) // num_uts_in_mts
+mts_feature_columns = list(mts_feature_df.columns)
+uts_pca_df_list = []
+for i in range(num_uts_in_mts):
+    uts_pca_transformer = PCAWrapper()
+    uts_columns = mts_feature_columns[i * num_uts_features : (i + 1) * num_uts_features]
+    uts_feature_df = mts_feature_df[uts_columns]
+    uts_pca_df = uts_pca_transformer.fit_transform(uts_feature_df)
+    uts_pca_df_list.append(uts_pca_df)
+
+logging.info("Successfully generated PCA spaces")
+
+# Add transofmed points to PCA spaces
+# FIXME: Add feature names to avoid warnings
+mts_features_pca_input = np.asarray(GA_runs_features[0]).reshape(tot_num_mts_features)
+transformed_mts_pca_df = mts_pca_transformer.transform(
+    # pd.DataFrame([mts_features_pca_input], columns=mts_feature_columns)
+    pd.DataFrame([mts_features_pca_input])
+)
+transformed_uts_pca_df_list = []
+for i in range(num_uts_in_mts):
+    uts_features_pca_input = GA_runs_features[0][i]
+    transformed_uts_pca_df = uts_pca_transformer.transform(
+        # pd.DataFrame(
+        #     [uts_features_pca_input],
+        #     columns=mts_feature_columns[
+        #         i * num_uts_features : (i + 1) * num_uts_features
+        #     ],
+        # )
+        pd.DataFrame(
+            [uts_features_pca_input],
+        )
+    )
+    transformed_uts_pca_df_list.append(transformed_uts_pca_df)
+
+# Estimate expected target regions in PCA space based on K-nearest neighbors
+k = 100
+
+init_uts_features = mts_feature_df[
+    mts_feature_columns[init_uts_index : init_uts_index + 4]
+].to_numpy()
+init_uts_feature_distances = np.linalg.norm(
+    init_uts_features - init_uts_features[init_mts_index], axis=1
+)
+distance_indices = np.argsort(init_uts_feature_distances)[:k]
+
+# Save results to pdf
+pdf_filename = os.path.join("src", "results", "experiment_results.pdf")
+with PdfPages(pdf_filename) as pdf:
+
+    # Plot original and transformed UTS
+    fig, axs = plt.subplots(2, 1, figsize=(10, 10))
+    fig.suptitle("Original and Transformed UTS")
+    axs[0].plot(init_uts, label="Original UTS")
+    axs[0].set_title(f"Original {init_uts_name}")
+    axs[0].legend()
+    axs[1].plot(transformed_uts, label="Transformed UTS")
+    axs[1].set_title(f"Transformed {init_uts_name}")
+    axs[1].legend()
+    pdf.savefig(fig)
+    plt.close()
+
+    # Plot original and transformed MTS
+    fig, axs = plt.subplots(num_uts_in_mts, 2, figsize=(10, 10))
+    fig.suptitle("Original and Transformed MTS")
+    for i in range(num_uts_in_mts):
+        uts_name = timeseries_to_use[i]
+        axs[i, 0].plot(init_mts[uts_name], label=f"{uts_name}")
+        axs[i, 0].set_title(f"Original {uts_name}")
+        axs[i, 0].legend()
+        axs[i, 1].plot(GA_runs_mts[0][i], label=f"t_{uts_name}")
+        axs[i, 1].set_title(f"Transformed {uts_name}")
+        axs[i, 1].legend()
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    pdf.savefig(fig)
+    plt.close()
+
+    # Plot PCA spaces
+    fig, axs = plt.subplots(4, 1, figsize=(10, 20))
+    fig.suptitle("PCA Spaces")
+    # All MTS points
+    axs[0].scatter(mts_pca_df["pca1"], mts_pca_df["pca2"], label="MTS")
+    axs[0].scatter(
+        mts_pca_df["pca1"][init_mts_index],
+        mts_pca_df["pca2"][init_mts_index],
+        color="yellow",
+        s=100,
+        edgecolors="black",
+        label="Original",
+    )
+    # Expected target region based on K-nearest neighbors
+    for mts_index in distance_indices:
+        axs[0].scatter(
+            mts_pca_df["pca1"][mts_index],
+            mts_pca_df["pca2"][mts_index],
+            color="green",
+            s=50,
+            alpha=0.5,
+        )
+    # The transformed MTS
+    axs[0].scatter(
+        transformed_mts_pca_df["pca1"].iloc[0],
+        transformed_mts_pca_df["pca2"].iloc[0],
+        color="red",
+        s=100,
+        edgecolors="black",
+        label="Transformed",
+    )
+
+    axs[0].set_title("MTS PCA Space")
+    axs[0].legend()
+
+    for i in range(num_uts_in_mts):
+        # All points for one UTS
+        uts_name = timeseries_to_use[i]
+        axs[i + 1].scatter(
+            uts_pca_df_list[i]["pca1"], uts_pca_df_list[i]["pca2"], label=f"{uts_name}"
+        )
+        # The initial UTS
+        axs[i + 1].scatter(
+            uts_pca_df_list[i]["pca1"][init_mts_index],
+            uts_pca_df_list[i]["pca2"][init_mts_index],
+            color="yellow",
+            s=100,
+            edgecolors="black",
+            label="Original",
+        )
+        # Expected target region based on K-nearest neighbors
+        if i == init_uts_index:
+            for mts_index in distance_indices:
+                axs[i + 1].scatter(
+                    uts_pca_df_list[i]["pca1"][mts_index],
+                    uts_pca_df_list[i]["pca2"][mts_index],
+                    color="green",
+                    s=50,
+                    alpha=0.5,
+                )
+        # The transformed UTS
+        axs[i + 1].scatter(
+            transformed_uts_pca_df_list[i]["pca1"].iloc[0],
+            transformed_uts_pca_df_list[i]["pca2"].iloc[0],
+            color="red",
+            s=100,
+            edgecolors="black",
+            label="Transformed",
+        )
+
+        axs[i + 1].set_title(f"{uts_name} PCA")
+        axs[i + 1].legend()
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    pdf.savefig(fig)
+    plt.close()
+
+logging.info(f"Results were succesfully saved to {pdf_filename}")
 
 # TODO: Load the pre-trained forecasting model
 # Perform forcasting on both original and transformed MTS
