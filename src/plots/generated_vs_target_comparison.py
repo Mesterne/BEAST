220bfe780379d3ebd714bfb0bcdccc3f160cd67e
Vegard Sjåvik <vgsjaavik@gmail.com>
2025-03-10 15:31:00 +0100

    Use median instead of random

--- src/plots/generated_vs_target_comparison.py
+++ src/plots/generated_vs_target_comparison.py
@@ -83,28 +83,30 @@ def create_and_save_plots_of_model_performances(
         os.path.join(OUTPUT_DIR, "worst_timeseries_generated_mts_pca.png")
     )
 
-    random_generated_mts_index = np.random.randint(len(total_mse_for_each_mts))
-    ts_plot_of_random_generated_mts = plot_time_series_for_all_uts(
-        original_mts=original_mts[random_generated_mts_index],
-        target_mts=target_mts[random_generated_mts_index],
-        transformed_mts=generated_mts[random_generated_mts_index],
-        original_mts_features=original_mts_features[random_generated_mts_index],
-        transformed_mts_features=transformed_mts_features[random_generated_mts_index],
-        target_mts_features=target_mts_features[random_generated_mts_index],
+    median_mse = np.median(total_mse_for_each_mts)
+    median_mts_index = np.where(total_mse_for_each_mts == median_mse)[0][0]
+
+    ts_plot_of_median_generated_mts = plot_time_series_for_all_uts(
+        original_mts=original_mts[median_mts_index],
+        target_mts=target_mts[median_mts_index],
+        transformed_mts=generated_mts[median_mts_index],
+        original_mts_features=original_mts_features[median_mts_index],
+        transformed_mts_features=transformed_mts_features[median_mts_index],
+        target_mts_features=target_mts_features[median_mts_index],
     )
-    ts_plot_of_random_generated_mts.savefig(
-        os.path.join(OUTPUT_DIR, "random_timeseries_generated_mts.png")
+    ts_plot_of_median_generated_mts.savefig(
+        os.path.join(OUTPUT_DIR, "median_timeseries_generated_mts.png")
     )
-    pca_plot_of_random_generated_mts = plot_pca_for_each_uts_with_transformed(
+    pca_plot_of_median_generated_mts = plot_pca_for_each_uts_with_transformed(
         mts_features_train=mts_features_train,
         mts_features_validation=mts_features_validation,
         mts_features_test=mts_features_test,
-        original_mts_features=original_mts_features[random_generated_mts_index],
-        target_mts_features=target_mts_features[random_generated_mts_index],
-        predicted_mts_features=transformed_mts_features[random_generated_mts_index],
+        original_mts_features=original_mts_features[median_mts_index],
+        target_mts_features=target_mts_features[median_mts_index],
+        predicted_mts_features=transformed_mts_features[median_mts_index],
     )
-    pca_plot_of_random_generated_mts.savefig(
-        os.path.join(OUTPUT_DIR, "random_timeseries_generated_mts_pca.png")
+    pca_plot_of_median_generated_mts.savefig(
+        os.path.join(OUTPUT_DIR, "median_timeseries_generated_mts_pca.png")
     )
 
     pca_total_plot = plot_pca_for_all_generated_mts(
