3db56b8b524a90585fb8b9114494d1f0e76666ce
Vegard Sjåvik <vgsjaavik@gmail.com>
2025-03-11 15:41:30 +0100

    Remove trend linearity from COLUMN NAMES

--- src/data/constants.py
+++ src/data/constants.py
@@ -3,15 +3,12 @@ import os
 COLUMN_NAMES = [
     "grid1-load_trend-strength",
     "grid1-load_trend-slope",
-    "grid1-load_trend-linearity",
     "grid1-load_seasonal-strength",
     "grid1-loss_trend-strength",
     "grid1-loss_trend-slope",
-    "grid1-loss_trend-linearity",
     "grid1-loss_seasonal-strength",
     "grid1-temp_trend-strength",
     "grid1-temp_trend-slope",
-    "grid1-temp_trend-linearity",
     "grid1-temp_seasonal-strength",
 ]
 
