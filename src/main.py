dad928b2a2ef425b93451c8232586018546ea4b9
Vegard Sjåvik <vgsjaavik@gmail.com>
2025-03-27 13:45:17 +0100

    Remove config setup from main.py

--- src/main.py
+++ src/main.py
@@ -31,7 +31,6 @@ from src.models.feature_transformation_model import FeatureTransformationModel
 from src.models.forecasting.feedforward import FeedForwardForecaster
 from src.models.model_handler import ModelHandler
 from src.models.neural_network_wrapper import NeuralNetworkWrapper
-from src.models.reconstruction.genetic_algorithm_wrapper import GeneticAlgorithmWrapper
 from src.plots.feature_distribution import plot_feature_distribution
 from src.utils.evaluation.evaluation import evaluate
 from src.utils.experiment_helper import (  # noqa: E402
@@ -64,42 +63,14 @@ plt.style.use("ggplot")
 
 # Load the configuration file
 config: Dict[str, Any] = read_yaml(args["config_path"])
-
 data_dir: str = os.path.join(
     config["dataset_args"]["directory"], config["dataset_args"]["training_data"]
 )
-timeseries_to_use: List[str] = config["dataset_args"]["timeseries_to_use"]
-step_size: int = config["dataset_args"]["step_size"]
-context_length: int = config["dataset_args"]["window_size"]
-num_features_per_uts: int = config["dataset_args"]["num_features_per_uts"]
-use_one_hot_encoding = config["dataset_args"]["use_one_hot_encoding"]
-test_set_sample_size = config["dataset_args"]["test_set_sample_size"]
-
-log_training_to_wandb: bool = config["training_args"]["log_to_wandb"]
-
-seasonal_period: int = config["stl_args"]["series_periodicity"]
-# FEATURE MODEL
-feature_model_params: Dict[str, Any] = config["model_args"]["feature_model_args"]
-model_type: str = feature_model_params["model_name"]
-genetic_algorithm_params: Dict[str, Any] = config["model_args"][
-    "genetic_algorithm_args"
-]
-# FORECAST MODEL
-forecasting_model_params: Dict[str, Any] = config["model_args"][
-    "forecasting_model_args"
-]
-forecasting_model_training_params: Dict[str, Any] = forecasting_model_params[
-    "training_args"
-]
-training_params: Dict[str, Any] = feature_model_params["training_args"]
-
-# Check if the feature model is a conditional generative model. If so, do necessary data preparation.
+num_uts_in_mts: int = len(config["dataset_args"]["timeseries_to_use"])
 config["is_conditional_gen_model"]: bool = (
     config["model_args"]["feature_model_args"]["conditional_gen_model_args"] is not None
 )
-
-# Set up logging to wandb
-if log_training_to_wandb:
+if config["training_args"]["log_to_wandb"]:
     job_name: str = os.environ.get("JOB_NAME", str(uuid.uuid4()))
     wandb.init(
         project="MTS-BEAST",
@@ -118,23 +89,15 @@ logger.info(
 # Load data and generate dataset of multivariate time series context windows
 mts_dataset_array: np.ndarray = get_mts_dataset(
     data_dir=data_dir,
-    time_series_to_use=timeseries_to_use,
-    context_length=context_length,
-    step_size=step_size,
+    time_series_to_use=config["dataset_args"]["timeseries_to_use"],
+    context_length=config["dataset_args"]["window_size"],
+    step_size=config["dataset_args"]["step_size"],
 )
-
-logger.info(
-    f"Reshaped multivariate time series dataset to shape: {mts_dataset_array.shape}"
-)
-
-dataset_size: int = len(mts_dataset_array)
-num_uts_in_mts: int = len(timeseries_to_use)
-logger.info(f"MTS Dataset shape: {mts_dataset_array.shape}")
 
 mts_features_array, mts_decomps = generate_feature_dataframe(
     data=mts_dataset_array,
-    series_periodicity=seasonal_period,
-    num_features_per_uts=num_features_per_uts,
+    series_periodicity=config["stl_args"]["series_periodicity"],
+    num_features_per_uts=config["dataset_args"]["num_features_per_uts"],
 )
 
 dist_of_features = plot_feature_distribution(mts_features_array)
@@ -147,42 +110,36 @@ mts_pca_array: np.ndarray = PCAWrapper().fit_transform(mts_features_array)
 logger.info("Successfully generated MTS PCA space")
 
 (
-    X_features_train,
-    y_features_train,
-    X_features_validation,
-    y_features_validation,
-    X_features_test,
-    y_features_test,
-    train_features_supervised_dataset,
-    validation_features_supervised_dataset,
-    test_features_supervised_dataset,
+    _,
+    _,
+    _,
+    _,
+    _,
+    _,
+    _,
+    _,
+    _,
     train_transformation_indices,
     validation_transformation_indices,
     test_transformation_indices,
 ) = create_train_val_test_split(
     mts_pca_array,
     mts_features_array,
-    use_one_hot_encoding=use_one_hot_encoding,
-    number_of_transformations_in_test_set=test_set_sample_size,
+    use_one_hot_encoding=config["dataset_args"]["use_one_hot_encoding"],
+    number_of_transformations_in_test_set=config["dataset_args"][
+        "test_set_sample_size"
+    ],
 )
 
 
-train_indices: List[int] = (
-    train_features_supervised_dataset["original_index"].astype(int).unique().tolist()
-)
-validation_indices: List[int] = (
-    validation_features_supervised_dataset["target_index"].astype(int).unique().tolist()
-)
-test_indices: List[int] = (
-    test_features_supervised_dataset["target_index"].astype(int).unique().tolist()
-)
+train_indices: List[int] = train_transformation_indices[:, 0]
+validation_indices: List[int] = validation_transformation_indices[:, 1]
+test_indices: List[int] = test_transformation_indices[:, 1]
 
 
-train_mts_array: np.ndarray = np.array([mts_dataset_array[i] for i in train_indices])
-validation_mts_array: np.ndarray = np.array(
-    [mts_dataset_array[i] for i in validation_indices]
-)
-test_mts_array: np.ndarray = np.array([mts_dataset_array[i] for i in test_indices])
+train_mts_array: np.ndarray = mts_dataset_array[train_indices]
+validation_mts_array: np.ndarray = mts_dataset_array[validation_indices]
+test_mts_array: np.ndarray = mts_dataset_array[test_indices]
 
 
 (
@@ -191,8 +148,8 @@ test_mts_array: np.ndarray = np.array([mts_dataset_array[i] for i in test_indice
 ) = create_training_windows_from_mts(
     mts=train_mts_array,
     target_col_index=1,
-    window_size=forecasting_model_params["window_size"],
-    forecast_horizon=forecasting_model_params["horizon_length"],
+    window_size=config["model_args"]["forecasting_model_args"]["window_size"],
+    forecast_horizon=config["model_args"]["forecasting_model_args"]["horizon_length"],
 )
 logger.info("Forecasting train data shape: {}".format(X_mts_train.shape))
 (
@@ -201,8 +158,8 @@ logger.info("Forecasting train data shape: {}".format(X_mts_train.shape))
 ) = create_training_windows_from_mts(
     mts=validation_mts_array,
     target_col_index=1,
-    window_size=forecasting_model_params["window_size"],
-    forecast_horizon=forecasting_model_params["horizon_length"],
+    window_size=config["model_args"]["forecasting_model_args"]["window_size"],
+    forecast_horizon=config["model_args"]["forecasting_model_args"]["horizon_length"],
 )
 logger.info("Forecasting validation data shape: {}".format(X_mts_validation.shape))
 (
@@ -211,41 +168,28 @@ logger.info("Forecasting validation data shape: {}".format(X_mts_validation.shap
 ) = create_training_windows_from_mts(
     mts=test_mts_array,
     target_col_index=1,
-    window_size=forecasting_model_params["window_size"],
-    forecast_horizon=forecasting_model_params["horizon_length"],
+    window_size=config["model_args"]["forecasting_model_args"]["window_size"],
+    forecast_horizon=config["model_args"]["forecasting_model_args"]["horizon_length"],
 )
 logger.info("Forecasting test data shape: {}".format(X_mts_test.shape))
 
 config["model_args"]["feature_model_args"]["number_of_uts_in_mts"] = num_uts_in_mts
-config["model_args"]["feature_model_args"][
-    "number_of_features_per_uts"
-] = num_features_per_uts
-config["model_args"]["feature_model_args"]["input_size"] = X_features_train.shape[1]
-# TODO :Delete
-feature_model_params["number_of_uts_in_mts"] = num_uts_in_mts
-feature_model_params["number_of_features_per_uts"] = num_features_per_uts
-feature_model_params["input_size"] = X_features_train.shape[1]
-
-########### MODEL INITIALIZATION
-feature_model: FeatureTransformationModel = get_feature_model_by_type(
-    model_type=model_type,
-    model_params=feature_model_params,
-    training_params=training_params,
+config["model_args"]["feature_model_args"]["number_of_features_per_uts"] = config[
+    "dataset_args"
+]["num_features_per_uts"]
+config["model_args"]["feature_model_args"]["input_size"] = (
+    num_uts_in_mts * config["dataset_args"]["num_features_per_uts"]
 )
-logger.info(f"Successfully initialized the {model_type} model")
-
-forecasting_model: FeedForwardForecaster = FeedForwardForecaster(
-    model_params=forecasting_model_params,
+config["model_args"]["feature_model_args"]["input_size"] += (
+    config["dataset_args"]["num_features_per_uts"] + num_uts_in_mts
+    if config["dataset_args"]["use_one_hot_encoding"]
+    else config["dataset_args"]["num_features_per_uts"] * num_uts_in_mts
 )
-
-forecasting_model_wrapper: NeuralNetworkWrapper = NeuralNetworkWrapper(
-    model=forecasting_model, training_params=forecasting_model_training_params
-)
-logging.info("Successfully initialized the forecasting model")
-
 
 model_handler = ModelHandler(config)
 model_handler.choose_model_category()
+
+############ TRAINING
 model_handler.train(
     mts_dataset=mts_dataset_array,
     train_transformation_indices=train_transformation_indices,
@@ -253,20 +197,7 @@ model_handler.train(
 )
 
 
-############ TRAINING
-
-logger.info("Training forecasting model...")
-forecasting_model_wrapper.train(
-    X_train=X_mts_train,
-    y_train=y_mts_train,
-    X_val=X_mts_train,
-    y_val=y_mts_train,
-    log_to_wandb=False,
-)
-
-
 ############ INFERENCE
-TARGET_NAMES = [f"target_{name}" for name in COLUMN_NAMES]
 logger.info("Running inference on validation set...")
 inferred_mts_validation = model_handler.infer(
     mts_dataset=mts_dataset_array,
@@ -281,10 +212,6 @@ inferred_mts_test = model_handler.infer(
 
 logger.info("Successfully ran inference on validation and test sets")
 
-
-print(f"Shape of inferred_mts_validation {inferred_mts_validation.shape}")
-print(f"Shape of inferred_mts_test {inferred_mts_test.shape}")
-
 evaluate(
     mts_array=mts_dataset_array,
     train_transformation_indices=train_transformation_indices,
@@ -295,27 +222,44 @@ evaluate(
 )
 
 
+# Forecasting model evaluations
 (
     X_transformed,
     y_transformed,
 ) = create_training_windows_from_mts(
     mts=(inferred_mts_validation),
     target_col_index=1,
-    window_size=forecasting_model_params["window_size"],
-    forecast_horizon=forecasting_model_params["horizon_length"],
+    window_size=config["model_args"]["forecasting_model_args"]["window_size"],
+    forecast_horizon=config["model_args"]["forecasting_model_args"]["horizon_length"],
 )
 
 X_new_train: np.ndarray = np.vstack((X_mts_train, X_transformed))
 y_new_train: np.ndarray = np.vstack((y_mts_train, y_transformed))
 
+forecasting_model_old: FeedForwardForecaster = FeedForwardForecaster(
+    model_params=config["model_args"]["forecasting_model_args"],
+)
+forecasting_model_wrapper_old: NeuralNetworkWrapper = NeuralNetworkWrapper(
+    model=forecasting_model_old,
+    training_params=config["model_args"]["forecasting_model_args"]["training_args"],
+)
+logger.info("Training forecasting model on old data")
+forecasting_model_wrapper_old.train(
+    X_train=X_mts_train,
+    y_train=y_mts_train,
+    X_val=X_mts_train,
+    y_val=y_mts_train,
+    log_to_wandb=False,
+)
 
 forecasting_model_new: FeedForwardForecaster = FeedForwardForecaster(
-    model_params=forecasting_model_params,
+    model_params=config["model_args"]["forecasting_model_args"],
 )
-
 forecasting_model_wrapper_new: NeuralNetworkWrapper = NeuralNetworkWrapper(
-    model=forecasting_model_new, training_params=forecasting_model_training_params
+    model=forecasting_model_new,
+    training_params=config["model_args"]["forecasting_model_args"]["training_args"],
 )
+logger.info("Training forecasting model on old + generated data")
 forecasting_model_wrapper_new.train(
     X_train=X_new_train,
     y_train=y_new_train,
@@ -331,7 +275,7 @@ model_comparison_fig = compare_old_and_new_model(
     y_val=y_mts_validation,
     X_train=X_mts_train,
     y_train=y_mts_train,
-    forecasting_model_wrapper_old=forecasting_model_wrapper,
+    forecasting_model_wrapper_old=forecasting_model_wrapper_old,
     forecasting_model_wrapper_new=forecasting_model_wrapper_new,
 )
 model_comparison_fig.savefig(
