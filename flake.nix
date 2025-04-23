a7d180bde8493292c5fc9591072f16b379eef4b5
Vegard Sjåvik <vgsjaavik@gmail.com>
2025-04-23 15:05:22 +0200

    Add flake.nix

--- flake.nix
+++ flake.nix
@@ -4,16 +4,16 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs = { self, nixpkgs }: {
-    devShells.default = let
-      pkgs = import nixpkgs { system = "aarch64-darwin"; };
-      micromamba-shell = pkgs.callPackage (pkgs.fetchFromGitHub {
-        owner = "micromamba";
-        repo = "micromamba-nix";
-        rev = "v0.2.12";
-        sha256 = "1qkvd9cpdfj2mb7xjmkg74iw8g9y7zha0gfp0wjyz0iyxzcrhv9i"; # Verified
-      }) {};
-    in {
-      default = micromamba-shell.override {
+    devShells = {
+      default = let
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };  # Specify the right system type here
+        micromamba-shell = pkgs.callPackage (pkgs.fetchFromGitHub {
+          owner = "micromamba";
+          repo = "micromamba-nix";
+          rev = "v0.2.12";
+          sha256 = "1qkvd9cpdfj2mb7xjmkg74iw8g9y7zha0gfp0wjyz0iyxzcrhv9i";  # Replace with correct hash
+        }) {};
+      in micromamba-shell.override {
         environmentFile = ./environment.yml;
       };
     };
